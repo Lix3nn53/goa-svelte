8032696c414ba7508971da73b01d5b1bfa54ee72
Lix3nn53 <lixakyildiz@gmail.com>
2022-05-07 05:45:51 +0300

    Imrpove design of store index

---
+++ src/lib/components/link/LinkButtonBlock.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+  export let href: string;
+  export let style: "primary" | "secondary" | "base" = "primary";
+  export let customClass = "";
+
+  let className = `button ${style} ${customClass}`;
+</script>
+
+<a {href} class={className}>
+  <div class="button-content"><slot /></div>
+</a>
+
+<style>
+  .button {
+    color: rgb(var(--color-base-900));
+    border: none;
+    padding: 0;
+    margin-left: 5px;
+    margin-right: 5px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-weight: 600;
+    position: relative;
+    display: inline-block;
+    border-top-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    box-shadow: 0 0 40px 0 rgba(0, 0, 0, 0.06);
+    cursor: pointer;
+  }
+
+  .button-content {
+    display: block;
+    transform: translate(6px, -6px);
+    transition: all 0.2s ease;
+    position: relative;
+    z-index: 10;
+    border-top-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    padding-top: 19px;
+    padding-right: 34px;
+    padding-bottom: 21px;
+    padding-left: 34px;
+  }
+
+  .button:before,
+  .button:after {
+    content: "";
+    display: block;
+    width: 8px;
+    height: 8px;
+    position: absolute;
+    transition: all 0.2s ease;
+  }
+
+  .button:before {
+    top: 0;
+    left: 0;
+    transform-origin: top left;
+    transform: rotate(-45deg) scale(1);
+  }
+
+  .button:after {
+    right: 0;
+    bottom: 0;
+    transform-origin: bottom right;
+    transform: rotate(45deg) scale(1);
+  }
+
+  .button:hover .button-content {
+    transform: translate(4px, -4px);
+  }
+
+  .button:hover:before {
+    transform: rotate(-45deg) scale(0.66);
+  }
+
+  .button:hover:after {
+    transform: rotate(45deg) scale(0.66);
+  }
+
+  .button:active .button-content {
+    transform: translate(0, 0);
+  }
+
+  .button:active:before {
+    transform: rotate(-45deg) scale(0);
+  }
+
+  .button:active:after {
+    transform: rotate(45deg) scale(0);
+  }
+
+  .primary,
+  .primary:before,
+  .primary:after {
+    background-color: rgb(var(--color-primary-700));
+  }
+
+  .button:disabled,
+  .button:disabled:before,
+  .button:disabled:after {
+    background-color: rgb(var(--color-base-200));
+  }
+
+  .secondary,
+  .secondary:before,
+  .secondary:after {
+    background-color: rgb(var(--color-secondary-800));
+  }
+
+  .primary .button-content {
+    background-color: rgb(var(--color-primary-500));
+  }
+
+  .button:disabled .button-content {
+    background-color: rgb(var(--color-base-400));
+  }
+
+  .secondary .button-content {
+    background: linear-gradient(
+      45deg,
+      rgb(var(--color-secondary-600)) 25%,
+      rgb(var(--color-secondary-500)) 100%
+    );
+  }
+</style>
--- src/routes/store/index.svelte
+++ src/routes/store/index.svelte
@@ -1,10 +1,4 @@
 <script context="module" lang="ts">
-  import CardStoreRank from "$lib/components/card/CardStoreRank.svelte";
-  import Tabs from "$lib/components/tabs/Tabs.svelte";
-  import TabList from "$lib/components/tabs/TabList.svelte";
-  import Tab from "$lib/components/tabs/Tab.svelte";
-  import TabPanel from "$lib/components/tabs/TabPanel.svelte";
-
   export const prerender = true;
 </script>
 
@@ -12,75 +6,69 @@
   <title>Guardians of Adelia - Store</title>
 </svelte:head>
 
-<section class="container flex flex-col items-center justify-center">
+<section class="flex flex-col items-center justify-center">
   <h2 class="text-3xl font-bold mb-4">Categories</h2>
   <div
-    class="container flex flex-wrap -mx-2 overflow-hidden h-96 w-full md:w-4/5 lg:w-3/4 xl:w-2/3"
+    class="container flex flex-wrap -mx-2 overflow-hidden h-96 w-full md:w-4/5 xl:w-2/3"
   >
     <div class="my-2 px-2 overflow-hidden w-full">
       <a
         href="/store/ranks"
-        class="btn flex items-end justify-start btn-secondary h-full w-full ranks"
+        class="btn flex items-end justify-start h-full w-full ranks duration-500 ease-in-out hover:no-underline"
       >
-        <span class="label capitalize text-xl">Ranks</span></a
+        <span class="label capitalize text-4xl font-bold">Ranks</span></a
       >
     </div>
 
-    <div class="my-2 px-2 lg:w-1/2 overflow-hidden w-full">
+    <div class="my-2 px-2 md:w-1/2 overflow-hidden w-full">
       <a
         href="/store/crates"
-        class="btn flex items-end justify-start btn-secondary h-full w-full crates"
+        class="btn flex items-end justify-start h-full w-full crates duration-500 ease-in-out hover:no-underline"
       >
-        <span class="label capitalize text-xl">Crates</span></a
+        <span class="label capitalize text-4xl font-bold">Crates</span></a
       >
     </div>
 
-    <div class="my-2 px-2 lg:w-1/2 overflow-hidden w-full">
+    <div class="my-2 px-2 md:w-1/2 overflow-hidden w-full">
       <a
         href="/store/boosts"
-        class="btn flex items-end justify-start btn-secondary h-full w-full boosts"
+        class="btn flex items-end justify-start h-full w-full boosts duration-500 ease-in-out hover:no-underline"
       >
-        <span class="label capitalize text-xl">boosts</span></a
+        <span class="label capitalize text-4xl font-bold">boosts</span></a
       >
     </div>
   </div>
 </section>
 
 <style>
+  .btn:hover {
+    transform: scale(0.98);
+  }
+
   .crates {
-    background: url("/img/store/crate_preview.png"),
-      linear-gradient(
-        345deg,
-        rgb(var(--color-secondary-400)) 5%,
-        rgb(var(--color-secondary-600)) 100%
-      );
+    background: url("/img/store/crate_preview_purple.png"),
+      linear-gradient(345deg, #a855f7 15%, #7c3aed 100%);
     background-size: contain;
     background-repeat: no-repeat;
-    background-position: right;
-    color: rgb(var(--color-secondary-900));
+    background-position: 95%;
+    color: rgb(var(--color-base-800));
   }
 
   .ranks {
     background: url("/img/store/ranks.png"),
-      linear-gradient(
-        345deg,
-        rgb(var(--color-secondary-400)) 5%,
-        rgb(var(--color-secondary-600)) 100%
-      );
+      linear-gradient(345deg, #facc15 15%, #df9920 100%);
+    background-size: contain;
     background-repeat: no-repeat;
-    background-position: 90% 80%;
-    color: rgb(var(--color-secondary-900));
+    background-position: 95%;
+    color: rgb(var(--color-base-900));
   }
 
   .boosts {
     background: url("/img/store/boost.png"),
-      linear-gradient(
-        345deg,
-        rgb(var(--color-secondary-400)) 5%,
-        rgb(var(--color-secondary-600)) 100%
-      );
+      linear-gradient(345deg, #9ecb15 15%, #578e0b 100%);
+    background-size: contain;
     background-repeat: no-repeat;
-    background-position: right;
-    color: rgb(var(--color-secondary-900));
+    background-position: 95%;
+    color: rgb(var(--color-base-800));
   }
 </style>
