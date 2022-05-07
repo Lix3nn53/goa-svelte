de5b679368e5f281f78240e4dca7d304a2a66b4a
Lix3nn53 <lixakyildiz@gmail.com>
2022-05-07 05:49:17 +0300

    fix build

---
+++ src/routes/store/crates.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  import CardStoreRank from "$lib/components/card/CardStoreRank.svelte";
+  import Tabs from "$lib/components/tabs/Tabs.svelte";
+  import TabList from "$lib/components/tabs/TabList.svelte";
+  import Tab from "$lib/components/tabs/Tab.svelte";
+  import TabPanel from "$lib/components/tabs/TabPanel.svelte";
+
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Guardians of Adelia - Store - Crates</title>
+</svelte:head>
+
+<div class="flex flex-row justify-center gap-8 pt-16">Crates</div>
